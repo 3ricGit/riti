ac9754c02891054f4565ce3d34d4f47c1b675a6f
chirchir <chirchir7370@gmail.com>
2020-04-01 12:05:39 +0300

    worked on db connection

--- processor/connectDB.php
+++ processor/connectDB.php
@@ -2,12 +2,12 @@
 
 session_start();
 $HOST = 'localhost';
-// $USER = 'ritiasso_root';
-// $PASSWORD = '^e8.6g[zJ5%.';
+$USER = 'ritiasso_root';
+$PASSWORD = '^e8.6g[zJ5%.';
 // $DB = 'ritiasso_riti';
-$USER = 'root';
-$PASSWORD = 'Chirchir1.';
-$DB = 'riti';
+// $USER = 'root';
+// $PASSWORD = 'Chirchir1.';
+// $DB = 'riti';
 
 // $port = '3306';
 

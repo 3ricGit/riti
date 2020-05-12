fd9869d508a239a3916c1823faebac99dfede9c0
chirchir <chirchir7370@gmail.com>
2020-05-12 13:40:51 +0300

    updated service worker v

--- sw.js
+++ sw.js
@@ -1,87 +1,94 @@
-const cacheName = 'cache-18';
+const cacheName = 'cache-v20';
 
 const filesToCache = [
-    '/',
-    'index.php',
-    'contact_us.php',
-    'apply.php',
-    'ventures.php',
-    '/about.php',
-    'css/bootstrap.css',
-    'css/fontawesome-all.css',
-    'css/swiper.css',
-    'css/magnific-popup.css',
-    'css/styles.css',
-    'images/intro.jpeg',
-    'js/jquery.min.js',
-    'js/popper.min.js',
-    'js/bootstrap.min.js',
-    'js/jquery.easing.min.js',
-    'js/swiper.min.js',
-    'js/jquery.magnific-popup.js',
-    'js/morphext.min.js',
-    'js/isotope.pkgd.min.js',
-    'js/validator.min.js',
-    'js/scripts.js',
-    'images/work.jpeg',
-    'images/bgHeader.jpeg',
-    'images/hexagon-green.svg',
-    'images/business.jpeg',
-    'images/down-arrow.png',
-    'webfonts/fa-solid-900.woff2',
-    'images/favicon.png',
-    'images/laboratory-clinic.jpg',
-    'images/physio9.jpg',
-    'images/construction.jpg',
-    'images/nurusing-home.jpeg',
-    'images/horticulture.jpg',
-    'images/uniform.jpg',
-    'images/img3.jpeg',
-    'images/medical%20clinic.jpg',
-    'images/food%20and%20diatetic.jpg',
-    'images/dental%20clinic.jpg',
-    'images/auto-garage.jpg',
-    'images/food%20supply.jpg',
-    'images/counselling%202.jpg',
-    'images/psychiatric.jpg',
-    'images/school-of-counselling.jpg',
-    'images/RITIBRONCHUREFINAL.jpg',
-    'images/RITILOGOFINAL(1).png',
-    '/images/marketing.jpg',
-    '/images/food.jpeg',
-    '/images/bg.jpg',
-    'images/clear.jpg',
-    '/membershipForm.docx'
-
-]
+  '/',
+  'index.php',
+  'contact_us.php',
+  'apply.php',
+  'ventures.php',
+  '/about.php',
+  'css/bootstrap.css',
+  'css/fontawesome-all.css',
+  'css/swiper.css',
+  'css/magnific-popup.css',
+  'css/styles.css',
+  'images/intro.jpeg',
+  'js/jquery.min.js',
+  'js/popper.min.js',
+  'js/bootstrap.min.js',
+  'js/jquery.easing.min.js',
+  'js/swiper.min.js',
+  'js/jquery.magnific-popup.js',
+  'js/morphext.min.js',
+  'js/isotope.pkgd.min.js',
+  'js/validator.min.js',
+  'js/scripts.js',
+  'images/work.jpeg',
+  'images/bgHeader.jpeg',
+  'images/hexagon-green.svg',
+  'images/business.jpeg',
+  'images/down-arrow.png',
+  'webfonts/fa-solid-900.woff2',
+  'images/favicon.png',
+  'images/laboratory-clinic.jpg',
+  'images/physio9.jpg',
+  'images/construction.jpg',
+  'images/nurusing-home.jpeg',
+  'images/horticulture.jpg',
+  'images/uniform.jpg',
+  'images/img3.jpeg',
+  'images/medical%20clinic.jpg',
+  'images/food%20and%20diatetic.jpg',
+  'images/dental%20clinic.jpg',
+  'images/auto-garage.jpg',
+  'images/food%20supply.jpg',
+  'images/counselling%202.jpg',
+  'images/psychiatric.jpg',
+  'images/school-of-counselling.jpg',
+  'images/RITIBRONCHUREFINAL.jpg',
+  'images/RITILOGOFINAL(1).png',
+  '/images/marketing.jpg',
+  '/images/food.jpeg',
+  '/images/bg.jpg',
+  'images/clear.jpg',
+  '/membershipForm.docx',
+];
 
 // service workers steps
 // install event
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
-    event.waitUntil(caches.open(cacheName).then(cache => {
-        return cache.addAll(filesToCache)
-    }));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 // activate event
 
 this.addEventListener('activate', function (event) {
-    var cachesToKeep = ['cache-v18'];
+  var cachesToKeep = ['cache-v20'];
 
-    event.waitUntil(caches.keys().then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-            if (cachesToKeep.indexOf(key) === -1) {
-                return caches.delete(key);
-            }
-        }));
-    }));
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (cachesToKeep.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
 // fetch event
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then(cacheResponse => {
-        return cacheResponse || fetch(event.request)
-    }));
-})
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      return cacheResponse || fetch(event.request);
+    })
+  );
+});
